135c229e0216c3109d1fbf4b9c47caaa427b7219
dkozlik <jedendaniel@gmail.com>
2026-01-03 16:06:48 +0100

    Fix h2 keyword error

--- src/main/java/com/ddd/cat/model/User.java
+++ src/main/java/com/ddd/cat/model/User.java
@@ -10,12 +10,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
 @Entity
+@Table(name = "\"user\"")
 public class User {
 
     @Id
